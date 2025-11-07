530c4a8e1eacf6d97283d3aa514733ac263ee251
Hugo Costa <hugocosta.devs@gmail.com>
2025-11-07 19:47:48 -0300

    conectando ao mongodb

--- src/main/java/com/api_payroll/payroll/resources/UserResources.java
+++ src/main/java/com/api_payroll/payroll/resources/UserResources.java
@@ -1,6 +1,8 @@
 package com.api_payroll.payroll.resources;
 
 import com.api_payroll.payroll.domain.User;
+import com.api_payroll.payroll.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,12 +15,13 @@ import java.util.List;
 @RestController
 @RequestMapping(value="/users")
 public class UserResources {
+
+    @Autowired
+    private UserService service;
+
     @RequestMapping(method= RequestMethod.GET)
     public ResponseEntity <List<User>> findAll(){
-        User hugo = new User("13", "HUGO", "maria@gmail.com");
-        User alex = new User("2", "Alex", "alex@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(hugo, alex));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
