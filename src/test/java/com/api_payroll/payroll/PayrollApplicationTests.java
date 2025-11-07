beb5ca4677bf71132e51257cb58e484e579fcdbd
Hugo Costa <hugocosta.oli@gmail.com>
2025-11-07 13:36:10 -0300

    first commit

---
+++ src/test/java/com/api_payroll/payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_payroll.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
