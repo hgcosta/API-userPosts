ec9d11afcd45c880600c139def81ad4b719fc918
Hugo Costa <hugocosta.devs@gmail.com>
2025-11-08 19:11:46 -0300

    create find by id method

--- src/main/java/com/api_payroll/payroll/service/UserService.java
+++ src/main/java/com/api_payroll/payroll/service/UserService.java
@@ -2,6 +2,7 @@ package com.api_payroll.payroll.service;
 
 import com.api_payroll.payroll.domain.User;
 import com.api_payroll.payroll.repository.UserRepository;
+import com.api_payroll.payroll.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +15,10 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findByID(String id){
+        return repo.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
