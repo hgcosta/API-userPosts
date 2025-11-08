fc423cc825777f27b4eadd64d434cf75ed65b566
Hugo Costa <hugocosta.devs@gmail.com>
2025-11-08 19:37:41 -0300

    create exception error id not found object

---
+++ src/main/java/com/api_payroll/payroll/resources/exception/ResourceExceptionHandle.java
@@ -0,0 +1,18 @@
+package com.api_payroll.payroll.resources.exception;
+
+import com.api_payroll.payroll.service.exception.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ResourceExceptionHandle {
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity <StandartError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandartError err = new StandartError(System.currentTimeMillis(),status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
---
+++ src/main/java/com/api_payroll/payroll/resources/exception/StandartError.java
@@ -0,0 +1,64 @@
+package com.api_payroll.payroll.resources.exception;
+
+import java.io.Serializable;
+
+public class StandartError implements Serializable {
+    public static final long serialVersionId = 1L;
+    private long timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+
+    public StandartError(){
+
+    }
+
+    public StandartError(long timestamp, Integer status, String error, String message, String path) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
