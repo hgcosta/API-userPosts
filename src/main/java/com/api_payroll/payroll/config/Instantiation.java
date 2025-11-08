8104a2b482aa5af7bbb8b24d2a28fa3e4adfe680
Hugo Costa <hugocosta.devs@gmail.com>
2025-11-07 21:55:28 -0300

    instance data base

---
+++ src/main/java/com/api_payroll/payroll/config/Instantiation.java
@@ -0,0 +1,24 @@
+package com.api_payroll.payroll.config;
+
+import com.api_payroll.payroll.domain.User;
+import com.api_payroll.payroll.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
